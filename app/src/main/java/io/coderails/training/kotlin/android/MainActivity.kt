60acb55eb4f42cfae6b8bcac23dea3cbc9b83f2f
Jungman Bae - iMac <jmbae@wbay.co.kr>
2019-01-19 11:34:17 +0900

    #2 두번째 실습

--- app/src/main/java/io/coderails/training/kotlin/android/MainActivity.kt
+++ app/src/main/java/io/coderails/training/kotlin/android/MainActivity.kt
@@ -1,12 +1,35 @@
 package io.coderails.training.kotlin.android
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mButton: Button
+
+    private val REQUEST_CODE_SECOND_ACTIVITY: Int = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mButton = findViewById(R.id.button)
+
+        mButton.setOnClickListener {
+            Toast.makeText(this, "Go second!", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("textFromMain", "Hello SecondActivity?")
+            startActivityForResult(intent, REQUEST_CODE_SECOND_ACTIVITY)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_SECOND_ACTIVITY && resultCode == Activity.RESULT_OK) {
+            textView.text = data?.getStringExtra("returnValue")
+        }
     }
 }
---
+++ app/src/main/java/io/coderails/training/kotlin/android/SecondActivity.kt
@@ -0,0 +1,27 @@
+package io.coderails.training.kotlin.android
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val stringFromMain = intent.getStringExtra("textFromMain")
+        val textView2 = findViewById<TextView>(R.id.textView2)
+
+        textView2.text = stringFromMain
+
+        button2.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("returnValue", "go back main")
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+    }
+}
